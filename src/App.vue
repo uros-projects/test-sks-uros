b71a5bdd6801ded57bb9fe7c5613baf136f4f59d
Uros <uros.tomasevic.work@gmail.com>
2021-04-23 16:20:53 +0200

    Added packages and a test component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <PostList />
+    </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostList from './components/PostList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PostList
   }
 }
 </script>
---
+++ src/components/PostList.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Test</h1>
+</template>
+
+<script>
+export default {
+    name: 'PostList'
+}
+</script>
+
+<style>
+
+</style>
